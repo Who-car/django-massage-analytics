24dc4618c2f0ab23fa3e6d179050b66b3b647cff
Who-car <aysulu2645@gmail.com>
2024-02-07 14:28:40 +0300

    add manual and auto session data generation

--- web/urls.py
+++ web/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     path('', views.main_view, name="main"),
     path('registration/', views.registration_view, name="registration"),
     path('auth/', views.auth_view, name="auth"),
-    path('logout/', views.logout_view, name="logout")
+    path('logout/', views.logout_view, name="logout"),
+    path('session/add', views.session_edit_view, name="add_session"),
+    path('session/<int:id>', views.session_edit_view, name="edit_session"),
+    path('session/<int:id>/delete', views.session_delete_view, name="delete_session"),
+    path("symptoms/", views.symptoms_view, name="symptoms"),
+    path("symtoms/<int:id>/delete/", views.symptoms_delete_view, name="symptoms_delete"),
+    path("massages/", views.massage_types_view, name="massages"),
+    path("massages/<int:id>/delete/", views.massage_types_delete_view, name="massages_delete"),
 ]
